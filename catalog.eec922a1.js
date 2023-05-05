25462bfbfb2e98f33549d1d3abd49aa295c6dd83
LightPride <32414745+LightPride@users.noreply.github.com>
2023-05-05 13:42:02 +0000

    Deploying to gh-pages from @ LightPride/Cinemania@220145eab2d97a54e9a1d72f2c389f3ae2f745b1 🚀

---
+++ catalog.eec922a1.js
@@ -0,0 +1,2 @@
+!function(){var a={catalogForm:document.querySelector("#search-form"),catalogGallery:document.querySelector(".catalog__gallery")};a.catalogForm,a.catalogGallery;a.catalogForm.addEventListener("submit",(function(a){a.preventDefault();a.currentTarget.catalogSearch}))}();
+//# sourceMappingURL=catalog.eec922a1.js.map
