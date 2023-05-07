836de7d0826c1f9a6252d281746521114140fb08
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2023-05-07 04:08:16 +0300

    Build

--- src/js/catalog-search.js
+++ src/js/catalog-search.js
@@ -22,28 +22,33 @@ function onCatalogFormSubmit(e) {
     return;
   }
 
-  fetchCatalogSearchMovies(inputValue).then(data => {
-    const movies = data.results.map(movie => {
-      const title = movie.title;
-      const posterPath = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      const releaseYear = movie.release_date.substring(0, 4);
-      const movieId = movie.id;
-      const rating = movie.popularity;
-      fetchInByMovieName(movie.genre_ids).then(genreArr => {
-        const genre = genreArr.map(gnr => gnr.name);
+  fetchMovieGenres().then(genres => {
+    fetchCatalogSearchMovies(inputValue).then(data => {
+      const movies = data.results.slice(0, 10);
+      movies.map(movie => {
+        const title = movie.title;
+        const posterPath = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        const releaseYear = movie.release_date.substring(0, 4);
+        const movieId = movie.id;
+        const rating = movie.vote_average;
+        const movieGenres = genres.genres.filter(genre => {
+          if (movie.genre_ids.includes(genre.id)) {
+            return genre.name;
+          }
+        });
+
         const cardsData = {
           title,
           posterPath,
-          genre,
+          movieGenres,
           releaseYear,
           rating,
           movieId,
         };
+
         console.log(cardsData);
       });
     });
-
-    console.log(movies);
   });
 }
 
@@ -52,7 +57,7 @@ function fetchCatalogSearchMovies(name) {
   const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
   return fetch(
-    `${BASE_URL}?api_key=${API_KEY}&language=en-US&page=1&query=${name}`
+    `${BASE_URL}?api_key=${API_KEY}&language=en-US&per_page=10&page=1&query=${name}`
   ).then(movieData => {
     if (!movieData.ok) {
       throw new Error(movieData.status);
@@ -62,33 +67,22 @@ function fetchCatalogSearchMovies(name) {
   });
 }
 
-function fetchInByMovieName(ids) {
+function fetchMovieGenres() {
   const API_KEY = 'ec3ca0e4403710b7fc1497b1dbf32c54';
   const BASE_URL = 'https://api.themoviedb.org/3/genre/movie/list';
 
-  return fetch(`${BASE_URL}?api_key=${API_KEY}`)
-    .then(movieData => {
-      if (!movieData.ok) {
-        throw new Error(movieData.status);
-      }
+  return fetch(`${BASE_URL}?api_key=${API_KEY}`).then(movieData => {
+    if (!movieData.ok) {
+      throw new Error(movieData.status);
+    }
 
-      return movieData.json();
-    })
-    .then(data => {
-      const searcedGenre = data.genres.filter(genre => {
-        // if (genre.id === id) {
-        if (ids.includes(genre.id)) {
-          return genre.name;
-        }
-      });
-
-      return searcedGenre;
-    });
+    return movieData.json();
+  });
 }
 
 function createGallery(movies) {
   const generatedHtml = movies
-    .map(({ title, posterPath, genre, releaseYear, rating, movieId }) => {
+    .map(({ title, posterPath, movieGenres, releaseYear, rating, movieId }) => {
       const htmlPart = `
         // 
         `;
@@ -105,34 +99,3 @@ function changeCurrentNavigation() {
   navigation[2].classList.remove('current');
   navigation[1].classList.add('current');
 }
-
-// ? from "fetchCatalogSearchMovies"
-// .then(data => {
-//   console.log(data.results);
-//   const movie = data.results[0];
-//   const movieId = movie.id;
-// getMoviesById(movieId).then(movie => {
-//   const title = movie.title;
-//   const posterPath = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-//   const genres = movie.genres.map(genre => genre.name);
-//   const releaseYear = movie.release_date.substring(0, 4);
-//   console.log(title, posterPath, genres, releaseYear);
-// });
-// });
-
-function getMoviesById(id) {
-  const API_KEY = 'ec3ca0e4403710b7fc1497b1dbf32c54';
-  const BASE_URL = `https://api.themoviedb.org/3/movie/603692`;
-
-  return fetch(`${BASE_URL}?api_key=${API_KEY}&language=en-US`)
-    .then(movieData => {
-      if (!movieData.ok) {
-        throw new Error(movieData.status);
-      }
-
-      return movieData.json();
-    })
-    .then(console.log);
-}
-
-getMoviesById();
