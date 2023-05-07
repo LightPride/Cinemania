b4b1062e019e8329704e34e44f3b868fc56d3e2b
Mykhaylo Yaremkevych <114567561+Mykhaylo33@users.noreply.github.com>
2023-05-07 21:24:44 +0300

    add html and js files

--- src/js/home-trends.js
+++ src/js/home-trends.js
@@ -1,136 +1,90 @@
-// const container = document.querySelector('.weekly-trends__insert');
+const container = document.querySelector('.weekly-trends__insert');
+const API_KEY = 'ec3ca0e4403710b7fc1497b1dbf32c54';
+const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
+
+async function fetchTrendsMovies() {
+    try {
+        const response = await fetch(`${BASE_URL}?api_key=${API_KEY}&language=en-US&page=1`);
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        const movieData = await response.json();
+        return movieData.results;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
-// const API_KEY = 'ec3ca0e4403710b7fc1497b1dbf32c54';
-// const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
+function getFetchedMovies() {
+    fetchTrendsMovies().then(movies => {
+        let count;
 
-// function fetchTrendsMovies() {
-//     //developers.themoviedb.org/3/trending/get-trending
-//     return fetch(`${BASE_URL}?api_key=${API_KEY}&language=en-US&page=1`)
-//         .then(movieData => {
-//             console.log(movieData);
-//             if (!movieData.ok) {
-//                 throw new Error(movieData.status)
-//             }
-//             return movieData.json()
+        if (window.matchMedia('(max-width: 767px)').matches) {
+            // mobile
+            count = 1;
+        } else if (window.matchMedia('(max-width: 1023px)').matches) {
+            // desktop
+            count = 3;
+        }
 
-
-//         })
-// }
-// fetchTrendsMovies()
+        renderMovies(movies, count);
+    }).catch(error => console.log(error));
+}
 
 
-// function renderMovies(movies, count) {
-//     container.innerHTML = '';
+getFetchedMovies();
 
-//     movies.slice(0, count).forEach(movie => {
-//         const li = document.createElement('li');
-//         const title = document.createElement('h3');
-//         const img = document.createElement('img');
-//         const rating = document.createElement('span');
+async function renderMovies(movies, count) {
+    const container = document.getElementById('movies-container');
+    container.innerHTML = '';
 
-//         title.innerText = movie.title;
-//         img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-//         rating.innerText = movie.vote_average;
+    movies.slice(0, count).forEach(async movie => {
+        const genres = await getGenresById(movie.genre_ids);
+        const li = document.createElement('li');
+        const movieInfo = document.createElement('div');
+        const title = document.createElement('h3');
+        const img = document.createElement('img');
+        const rating = document.createElement('span');
+        const releaseDate = document.createElement('span');
+        const genre = document.createElement('span');
 
-//         li.appendChild(title);
-//         li.appendChild(img);
-//         li.appendChild(rating);
-//         container.appendChild(li);
-//     });
-// }
+        title.innerText = `${movie.title}`;
+        genre.innerText = `${genres}`;
+        releaseDate.innerText = `${movie.release_date.slice(0, 4)}`;
+        rating.innerText = `${movie.vote_average.toFixed(1)}`;
+        img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        img.alt = `${movie.title} movie poster`;
 
-// function getFetchedMovies() {
-//     fetchTrendsMovies().then(data => {
-//         const movies = data.results;
-//         let count;
+        movieInfo.appendChild(title);
+        movieInfo.appendChild(genre);
+        movieInfo.appendChild(releaseDate);
 
-//         if (window.matchMedia('(max-width: 767px)').matches) {
-//             // mobile
-//             count = 1;
-//         } else if (window.matchMedia('(max-width: 1023px)').matches) {
-//             // tablet
-//             count = 2;
-//         } else {
-//             // desktop
-//             count = 3;
-//         }
-
-//         renderMovies(movies, count);
-//     }).catch(error => console.log(error));
-// }
-
-// getFetchedMovies();
-
-// // Listen to resize events and update the rendered movies
-// window.addEventListener('resize', getFetchedMovies);
+        li.appendChild(img);
+        li.appendChild(movieInfo);
+        li.appendChild(rating);
+        container.appendChild(li);
+    });
+}
 
 
-// // // ================================================
-// // // const { createProxyMiddleware } = require('http-proxy-middleware');
+async function getGenresById(genreIds) {
+    const API_KEY = 'ec3ca0e4403710b7fc1497b1dbf32c54';
+    const BASE_URL = `https://api.themoviedb.org/3/genre/movie/list`;
 
-// // // module.exports = function (app) {
-// // //     app.use(
-// // //         '/api',
-// // //         createProxyMiddleware({
-// // //             target: 'https://api.themoviedb.org/3',
-// // //             changeOrigin: true,
-// // //         })
-// // //     );
-// // // };
+    const response = await fetch(`${BASE_URL}?api_key=${API_KEY}&language=en-US`);
+    const data = await response.json();
 
-// // // =================================================
+    const genreNames = genreIds.map((genreId) => {
+        const genre = data.genres.find((genre) => genre.id === genreId);
+        return genre.name;
+    });
 
+    return genreNames.join(', ');
+}
 
-// // const express = require('express');
-// // const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// // const app = express();
-
-// // // Налаштування проксі-сервера для запитів до API-сервера
-// // app.use('/api', createProxyMiddleware({
-// //     target: 'https://api.themoviedb.org',
-// //     changeOrigin: true,
-// //     pathRewrite: {
-// //         '^/api': '/3/trending/movie/week' // замінюємо шлях запиту з '/api' на '/3/trending/movie/week'
-// //     },
-// //     headers: {
-// //         'Authorization': 'Bearer <your-api-key>',
-// //         'Content-Type': 'application/json',
-// //     }
-// // }));
-
-// // // Ваші маршрути та інші налаштування сервера
-
-// // // Запускаємо сервер
-// // app.listen(3000, () => {
-// //     console.log('Server is running on http://localhost:3000');
-// // });
-
-// // !Перевірка на помилку
-
-// // fetch('https://jsonplaceholder.typicode.com/users')
-// //     .then(response => response.json())
-// //     .then(data => console.log(data))
-// //     .catch(error => console.error(error))
+window.addEventListener('resize', () => {
+    getFetchedMovies();
+});
 
 
-
-
-// // fetch('https://jsonplaceholder.typicode.com/users')
-// //     .then(response => response.json())
-// //     .then(data => {
-// //         const userList = document.querySelector('.user-list');
-// //         let output = '';
-// //         data.forEach(user => {
-// //             output += `
-// //         <li>
-// //           <h3>${user.name}</h3>
-// //           <p>${user.email}</p>
-// //         </li>
-// //       `;
-// //         });
-// //         userList.innerHTML = output;
-// //     })
-// //     .catch(error => console.error(error));
-//  ===============
