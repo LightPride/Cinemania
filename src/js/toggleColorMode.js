69123ca8c990a105386f4d07b95e017abf449304
IYuliia <catnew2012@gmail.com>
2023-05-08 14:49:22 +0200

    saving the color theme choice to local storage

--- src/js/toggleColorMode.js
+++ src/js/toggleColorMode.js
@@ -2,21 +2,27 @@ const darkModeIcon = document.querySelector('.dark-mode__icon');
 const lightModeIcon = document.querySelector('.light-mode__icon');
 const toggleButton = document.querySelector('.toggleButton');
 
-toggleButton.addEventListener('click', () => {
-  toggleIconDisplay();
-  toggleTheme();
-});
-
-function toggleIconDisplay() {
-  if (darkModeIcon.style.display === 'none') {
-    darkModeIcon.style.display = 'block';
-    lightModeIcon.style.display = 'none';
-  } else {
+const applyTheme = isLightMode => {
+  if (isLightMode) {
     darkModeIcon.style.display = 'none';
     lightModeIcon.style.display = 'block';
+    document.body.classList.add('light-theme');
+  } else {
+    darkModeIcon.style.display = 'block';
+    lightModeIcon.style.display = 'none';
+    document.body.classList.remove('light-theme');
   }
-}
+};
 
-function toggleTheme() {
-  document.body.classList.toggle('light-theme');
+const toggleTheme = () => {
+  const isLightMode = document.body.classList.contains('light-theme');
+  localStorage.setItem('isLightMode', !isLightMode);
+  applyTheme(!isLightMode);
+};
+
+toggleButton.addEventListener('click', toggleTheme);
+
+const isLightMode = JSON.parse(localStorage.getItem('isLightMode'));
+if (isLightMode !== null) {
+  applyTheme(isLightMode);
 }
