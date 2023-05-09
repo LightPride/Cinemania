162fbca7fcc1165aa77a0e4116bd92cbdf56be80
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-05-09 09:09:37 +0300

    Alex

--- src/js/mogal-popup.js
+++ src/js/mogal-popup.js
@@ -20,11 +20,11 @@ const classes = {
   openModal: 'open-modal',
   visual: 'visual',
 };
-
+// ВИКЛИК МОДАЛКИ ТИМЧАСОВОЮ КНОПКОЮ (ПОТІМ ПРИБРАТИ)
 refs.openModalPopUp.addEventListener('click', handlePopUpModal);
+
 refs.closeModalPopUp.addEventListener('click', handlePopUpModal);
 refs.overlayPopUp.addEventListener('click', handlePopUpModal);
-
 document.addEventListener('keydown', handlePopUpModalClose)
 
 function handlePopUpModalClose({code}) {  
@@ -42,7 +42,57 @@ function handlePopUpModal() {
   console.log(modalPopUp);  
 };
 
-// ===== Перемикач теми =====
+// === Тимчасовий пробний запис в localStorage ===
+// localStorage.setItem('filmId', 502356);
+// localStorage.removeItem('filmId', 502356);
+// POPUP MOVIES
+const API_KEY = 'ec3ca0e4403710b7fc1497b1dbf32c54';
+const POPUP_URL = `https://api.themoviedb.org/3/movie/`;
+const POPUP_ID = localStorage.filmId;
+let MYLIBRARY_ID;
+console.log(POPUP_ID);
+// 502356 840326 1008005
+
+function fetchPopUpMovies() {
+  return fetch(`${POPUP_URL}${POPUP_ID}?api_key=${API_KEY}`)
+  .then(data => {    
+    return data.json();      
+  })
+}
+
+async function getPopUpMovies() {
+  try {
+    const { id, poster_path, title, overview, popularity, vote_average, vote_count, genres } = await fetchPopUpMovies();
+    MYLIBRARY_ID = id;
+    console.log(MYLIBRARY_ID);
+    refs.image.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;      
+    refs.titles.textContent = title;            
+    refs.vote.textContent = vote_average;         
+    refs.votes.textContent = vote_count;      
+    refs.popular.textContent = popularity;    
+    console.log(genres);       
+    refs.genre.textContent = genres.map((genres) => genres.name).join(" ");   
+    refs.aboutTxtPopUp.textContent = overview;      
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// === set/remome в localStorage 'mylberyId' ====
+refs.btnPopUp.addEventListener('click', pushMyLibrary);
+
+function pushMyLibrary() {
+  if (!refs.btnPopUp.classList.contains('add_mylibrary')) {
+    refs.btnPopUp.classList.add('add_mylibrary');     
+    localStorage.setItem('mylberyId', MYLIBRARY_ID);  
+    console.log(localStorage.mylberyId);
+  } else {
+    refs.btnPopUp.classList.remove('add_mylibrary');
+    localStorage.removeItem('mylberyId', MYLIBRARY_ID);
+  }
+}
+
+// ===== Перемикач теми DARK/LIGHT =====
 const toogle = document.getElementById('toggle');
 toogle.addEventListener('click', toogleLight);
 
@@ -67,32 +117,3 @@ function toogleLightPopUp() {
   refs.aboutTxtPopUp.classList.remove('light_color');  
  }
 };
-
-// POPUP MOVIES
-const API_KEY = 'ec3ca0e4403710b7fc1497b1dbf32c54';
-const POPUP_URL = `https://api.themoviedb.org/3/movie/`;
-const POPUP_ID = 840326;
-// 502356 840326 1008005
-
-function fetchPopUpMovies() {
-  return fetch(`${POPUP_URL}${POPUP_ID}?api_key=${API_KEY}`)
-  .then(data => {    
-    return data.json();      
-  })
-}
-
-async function getPopUpMovies() {
-  try {
-    const { poster_path, title, overview, popularity, vote_average, vote_count, genres } = await fetchPopUpMovies();
-    refs.image.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;      
-    refs.titles.textContent = title;            
-    refs.vote.textContent = vote_average;         
-    refs.votes.textContent = vote_count;      
-    refs.popular.textContent = popularity;    
-    console.log(genres);       
-    refs.genre.textContent = genres.map((genres) => genres.name).join(" ");   
-    refs.aboutTxtPopUp.textContent = overview;      
-  } catch (error) {
-    console.log(error);
-  }
-}
