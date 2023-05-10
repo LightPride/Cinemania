4fab01ccd634bd4f1f3dc82276c3e876031473ad
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-05-10 12:12:21 +0200

    added library

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -1,61 +1,106 @@
 const refs = {
-upcomingBtn: document.querySelector('#upcoming_btn'),
-addToLibraryBtn: document.getElementById('mylibrary'),
-libraryList: document.getElementById('library_list'),
-}
+  upcomingBtn: document.querySelector('#upcoming_btn'),
+  addToLibraryBtn: document.getElementById('mylibrary'),
+  libraryList: document.getElementById('library_list'),
+  catalogGallery: document.querySelector('.catalog__gallery'),
+};
+
+
+
+const savedSettings = localStorage.getItem('myLibraryIds');
+const parsedSettings = JSON.parse(savedSettings);
+const filmsIds = parsedSettings.idsArray;
 
 const API_KEY = 'ec3ca0e4403710b7fc1497b1dbf32c54';
 const POPUP_URL = `https://api.themoviedb.org/3/movie/`;
 
-refs.addToLibraryBtn.addEventListener('click', onAddToLibraryBtnClick);
-
-function onAddToLibraryBtnClick (e) {
-    console.log('click on add to library btn');
-
-    fetchFilmById().then(data => {
-        const film = {
-          title: data.title,
-          posterPath: data.poster_path,
-          genres: data.genres,
-          releaseYear: data.release_date.substring(0, 4),
-          id: data.id,
-          voteAverage: data.vote_average,
-        };
-      
-        console.log(film); // Вывести объект фильма в консоль
-
-        console.log(markup(film));
-        console.log(refs.libraryList);
-        refs.libraryList.insertAdjacentHTML('beforeend', markup(film));
-      });
-
-
+function renderLibrary(array) {
+  refs.catalogGallery.innerHTML = "";
+  array.map(id => {
+    fetchPopUpMovies(id).then(async data => {
+      if(!createGallery(data)) {
+        return;
+      }
+      refs.catalogGallery.insertAdjacentHTML('beforeend', createGallery(data));
+    });
+  });
 }
 
-function fetchFilmById() {
-    return fetch(`${POPUP_URL}${localStorage.getItem('mylbery-id')}?api_key=${API_KEY}`)
-    .then(data => {    
-      return data.json()  
-    })
+function fetchPopUpMovies(id) {
+  return fetch(`${POPUP_URL}${id}?api_key=${API_KEY}`).then(data => {
+    return data.json();
+  });
+}
+
+renderLibrary(filmsIds);
+
+function createGallery({ title, poster_path, release_date, vote_average, genres }) {
+  if(!genres[0]) {
+    return;
   }
-
-
-
-function markup(film) {
-
-    return `
-    <li class="movie__card" data-id="${film.id}">
+  const genre = genres[0].name;
+  // console.log(genre);
+  const htmlPart = `
+        <li class="movie__card">
         <div class="movie__card-poster" style="background-image: linear-gradient(rgba(0,0,0,0),rgba(0, 0, 0, 0), rgba(0,0,0,1)),
-  url(${film.poster_path});" data-id="${film.id}"></div>
+  url(https://image.tmdb.org/t/p/w500/${poster_path});"></div>
 
-        <div class="movie__card-info" data-id="${film.id}">
-          <h3 data-id="${film.id}">${film.title}</h3>
-          <p data-id="${film.id}"><span data-id="${film.id}">${film.genres}</span> | <span data-id="${film.id}">${film.release_date}</span></p>
+        <div class="movie__card-info">
+          <h3>${title}</h3>
+          <p><span>${genre}</span> | <span>${release_date.substring(0, 4)}</span></p>
         </div>
 
-        <div class="movie__card-rating" data-id="${film.id}">${film.vote_average}</div>
-      </li>
-    `
+        <div class="movie__card-rating">${vote_average}</div>
+      </li> 
+        `;
+
+  return htmlPart;
 }
 
-console.log(refs.libraryList);
+// function onAddToLibraryBtnClick (e) {
+//     console.log('click on add to library btn');
+//     let array = [];
+
+//     fetchFilmById().then(data => {
+//         const movies = {
+//           title: data.title,
+//           posterPath: data.poster_path,
+//           genres: data.genres,
+//           releaseYear: data.release_date,
+//           id: data.id,
+//           voteAverage: data.vote_average,
+//         }
+
+//         console.log(movies); // Вывести объект фильма в консоль
+
+//       //   for(const movie in movies) {
+//       //     console.log(
+//       //       `
+//       //       <li class="movie__card" data-id="${movie.id}">
+//       //       <div class="movie__card-poster" style="background-image: linear-gradient(rgba(0,0,0,0),rgba(0, 0, 0, 0), rgba(0,0,0,1)),
+//       // url(${movie.poster_path});" data-id="${movie.id}"></div>
+
+//       //       <div class="movie__card-info" data-id="${movie.id}">
+//       //         <h3 data-id="${movie.id}">${movie.title}</h3>
+//       //         <p data-id="${movie.id}"><span data-id="${movie.id}">${movie.genres}</span> | <span data-id="${movie.id}">${movie.release_date}</span></p>
+//       //       </div>
+
+//       //       <div class="movie__card-rating" data-id="${movie.id}">${movie.vote_average}</div>
+//       //     </li>
+
+//       //       `
+//       //     )
+//       //   }
+
+//       });
+
+// }
+
+// function fetchFilmById() {
+//     return fetch(`${POPUP_URL}${localStorage.getItem('mylbery-id')}?api_key=${API_KEY}`)
+//     .then(data => {
+//       return data.json()
+//     })
+//   }
+
+// fetchFilmById().then(console.log)
